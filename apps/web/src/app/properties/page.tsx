9a841ddb0b993d9bf4765ff85fbfa98a5a32293b
baso_afriza <134659802+Baso-CODE@users.noreply.github.com>
2024-03-20 10:01:13 +0700

    Rm 37 property management (#18)
    * chore: initial commit
    
    * fix: error
    
    * feat: property management

--- apps/web/src/app/properties/page.tsx
+++ apps/web/src/app/properties/page.tsx
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PropertyCard from "./components/PropertyCard";
+=======
 "use client";
+>>>>>>> 4c53071c2f38b9f23d1e0b1d050363302248a1a2
 
 import { baseUrl } from "@/utils/config";
 import axios from "axios";
@@ -11,11 +17,21 @@ import PropertyCard from "./components/PropertyCard";
 import TopDestination from "@/components/TopDestination";
 const Property = () => {
   const [properties, setProperties] = useState([]);
+<<<<<<< HEAD
+=======
   const searchParams = useSearchParams();
+>>>>>>> 4c53071c2f38b9f23d1e0b1d050363302248a1a2
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+<<<<<<< HEAD
+        const response = await axios.get(
+          "http://localhost:8000/api/property/location/date"
+        );
+        setProperties(response.data.properties);
+        console.log("ini adalah data  dari setProperties", setProperties);
+=======
         const location = searchParams.get("location") || "";
         const startDate = searchParams.get("startDate") || "";
         const endDate = searchParams.get("endDate") || "";
@@ -24,12 +40,26 @@ const Property = () => {
           `${baseUrl}/property/location/date?location=${location}&startDate=${startDate}&endDate=${endDate}&guest=${guest}`
         );
         setProperties(response.data.properties);
+>>>>>>> 4c53071c2f38b9f23d1e0b1d050363302248a1a2
       } catch (error) {
         console.error("Error fetching properties:", error);
       }
     };
 
     fetchData();
+<<<<<<< HEAD
+  }, []);
+  return (
+    <div>
+      <div className=" pt-[5rem] bg-gray-200 pb-[4rem]">
+        <h1 className=" heading">Best Room Property</h1>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center w-[80%] mx-auto mt-[4rem]">
+          {properties.map((property) => (
+            <div key={property}>
+              <PropertyCard property={property} />
+            </div>
+          ))}
+=======
   }, [searchParams]);
 
   return (
@@ -73,6 +103,7 @@ const Property = () => {
               <TopDestination />
             </div>
           </div>
+>>>>>>> 4c53071c2f38b9f23d1e0b1d050363302248a1a2
         </div>
       </div>
     </>
