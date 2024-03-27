52157e602ff8117bcb79e8c72e17f10315f00cb4
Baso-CODE <basoafrizamuhafriza@gmail.com>
2024-03-27 14:35:58 +0700

    feat: fix error

--- apps/api/src/controllers/room/getRoomByPropertyId.controller.ts
+++ apps/api/src/controllers/room/getRoomByPropertyId.controller.ts
@@ -4,8 +4,8 @@ import { Request, Response, NextFunction } from "express";
 export class GetRoomByPropertyIdController {
   async getRoomsByPropertyId(req: Request, res: Response, next: NextFunction) {
     try {
-      const { propertyId } = req.params;
-      const rooms = await getRoomsByPropertyIdAction(parseInt(propertyId));
+      const { id } = req.params;
+      const rooms = await getRoomsByPropertyIdAction(parseInt(id));
       return res.status(rooms.status).json({
         message: rooms.message,
         rooms: rooms.rooms,
