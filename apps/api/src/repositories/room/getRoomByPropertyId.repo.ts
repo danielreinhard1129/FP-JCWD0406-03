1174fc27fd3e9cff3050084dd071ed36ec9bed0f
baso_afriza <134659802+Baso-CODE@users.noreply.github.com>
2024-03-27 14:36:36 +0700

    Rm 42 fix bug 2 (#29)
    * chore: initial commit
    
    * fix: error
    
    * feat: fix error

--- apps/api/src/repositories/room/getRoomByPropertyId.repo.ts
+++ apps/api/src/repositories/room/getRoomByPropertyId.repo.ts
@@ -4,6 +4,9 @@ export const getRoomsByPropertyIdRepo = async (propertyId: number) => {
   try {
     const rooms = await prisma.room.findMany({
       where: { propertyId },
+      include: {
+        images: true,
+      },
     });
     return rooms;
   } catch (error) {
